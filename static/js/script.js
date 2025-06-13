9acb3356ada33ab871787b4c04829f9317f067cd
J.M.E <joaquin.munoz91@gmail.com>
2025-06-12 23:24:39 -0400

    cambios en círculos y pruebas con z-index

--- static/js/script.js
+++ static/js/script.js
@@ -5,3 +5,34 @@ document.addEventListener("mousemove", function(e) {
   document.body.style.setProperty('--x', `${x}%`);
   document.body.style.setProperty('--y', `${y}%`);
 });
+
+
+
+const circulos = document.querySelectorAll('.circulo');
+
+circulos.forEach((circulo, index) => {
+  let angle = 0;
+  let animationFrameId = null;
+
+  const velocidades = [15, 12, 9, 6, 3];
+  const velocidad = velocidades[index] || 2;
+
+  function rotate() {
+    angle = (angle + velocidad) % 360;
+    circulo.style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+    animationFrameId = requestAnimationFrame(rotate);
+  }
+
+  circulo.addEventListener('mouseenter', () => {
+    if (animationFrameId === null) {
+      rotate();
+    }
+  });
+
+  circulo.addEventListener('mouseleave', () => {
+    if (animationFrameId !== null) {
+      cancelAnimationFrame(animationFrameId);
+      animationFrameId = null;
+    }
+  });
+});
