61a87a46c415a035d15d10c92a6269699d5036ef
J.M.E <joaquin.munoz91@gmail.com>
2025-06-12 20:07:51 -0400

    agregado fondo interactivo sencillo y favicon

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+console.log("Script JS cargado correctamente");
+document.addEventListener("mousemove", function(e) {
+  const x = (e.clientX / window.innerWidth) * 100;
+  const y = (e.clientY / window.innerHeight) * 100;
+  document.body.style.setProperty('--x', `${x}%`);
+  document.body.style.setProperty('--y', `${y}%`);
+});
