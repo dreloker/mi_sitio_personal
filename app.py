497fd98cfafe68ea66beb52355a1cc078e43bf22
J.M.E <joaquin.munoz91@gmail.com>
2025-06-12 20:24:39 -0400

    solucionado problema de lectura de la pag de servicios

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def index():
      return render_template('index.html')
 
-@app.route('/')
+@app.route('/services')
 def services():
      return render_template('services.html')
 
